62707a2a828f1732c94028df4993e702421e8d54
Eric <eric275j@gmail.com>
2024-05-02 20:46:13 -0400

    Initial Commit

---
+++ LightsController/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LightsController
+//
+//  Created by Eric Johns on 5/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LightsController/LightsControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightsControllerApp.swift
+//  LightsController
+//
+//  Created by Eric Johns on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct LightsControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
